2450ea5857a16ddc4742e4777c37f56646116b14
beobeb <114521210+beobeb@users.noreply.github.com>
2023-11-03 21:15:13 +0100

    zaj_1

---
+++ App1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Linq;
+
+namespace App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("input number:");
+            number = int.Parse(Console.ReadLine());
+
+            while (number != 0)
+            {
+                Console.WriteLine("input number");
+                number = int.Parse(Console.ReadLine());
+            }
+
+            if(number == 0)
+            {
+                Console.WriteLine("pozostaly 2 proby");
+                number = int.Parse(Console.ReadLine());
+                if (number == 0)
+                {
+                    Console.WriteLine("pozostala 1 proba");
+                    number = int.Parse(Console.ReadLine());
+                    if (number == 0)
+                    {
+                        Console.WriteLine("Program zakonczony");
+                    }
+                }
+            }
+
+        }
+    }
+}
